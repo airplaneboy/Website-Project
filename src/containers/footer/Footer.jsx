78f923f6fba3cc80ecef495d351d29881128c5a6
Agara Sulaiman <agarasualimany@gmail.com>
2023-03-03 05:26:58 +0100

    Added Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 import './footer.css';
+import { Typography } from '@mui/material';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%',
+          justifyContent: 'center',
+          height: '5em',
+          background: '#0d0d0d',
+          mt: 10,
+          position: 'absolute',
+        }}>
+        <Typography>Footer Section</Typography>
+      </Box>
+    </footer>
+  );
 };
 
 export default Footer;
