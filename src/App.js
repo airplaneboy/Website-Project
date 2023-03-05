b2a414d48017532d3642fadde33e4f7c0ead154a
Agara Sulaiman <agarasualimany@gmail.com>
2023-03-05 02:58:11 +0100

    Fixed minor bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './app.css';
+import './App.css';
 import { Navbar } from './components';
 import { IconContext } from 'react-icons/lib';
 import Theme from './theme';
